896a2949677ff3e9ab2345f5784d416b64711f9f
Sanket Mahadik <sanketmahadik1999@gmail.com>
2024-03-08 22:32:21 +0530

    colorpicker react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const initialColors = Array.from({ length: 12 }, () => getRandomColor());
+  const [colors, setColors] = useState(initialColors);
+
+  function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  function handleCardClick(index) {
+    const newColors = [...colors];
+    newColors[index] = getRandomColor();
+    setColors(newColors);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="RandomColorCards-container">
+    {colors.map((color, index) => (
+      <div
+        key={index}
+        className="Card"
+        style={{ backgroundColor: color }}
+        onClick={() => handleCardClick(index)}
+      >
+        <span className="Card-colorText">{color}</span>
+      </div>
+    ))}
+  </div>
+);
 }
 
 export default App;
